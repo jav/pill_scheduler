ded4afede58d86120c6f4b87058d477dc3355617
jav <javier@ubillos.org>
2020-01-03 12:18:02 +0100

    Moved 'HomeScreen' to directory 'Screens'.

--- App.tsx
+++ App.tsx
@@ -2,17 +2,9 @@ import React from 'react';
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
 
-import { StyleSheet, Text, View } from 'react-native';
+import HomeScreen from './Screens/HomeScreen'
 
-class HomeScreen extends React.Component {
-  render() {
-    return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>Home Screen</Text>
-      </View>
-    );
-  }
-}
+import { StyleSheet } from 'react-native';
 
 const AppNavigator = createStackNavigator({
   Home: {
---
+++ Screens/HomeScreen.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+
+import { StyleSheet, Text, View } from 'react-native';
+
+class Props {}
+
+export default class HomeScreen extends React.Component<Props> {
+    render() {
+        return (
+            <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+                <Text>Home Screen</Text>
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+});
