86f886070f4774a9c26709cef287293772e6a020
jav <javier@ubillos.org>
2020-01-03 12:14:22 +0100

    Added first React Native Navigation screen
    Note: Lots of cargoculting in this commit.
    Installing dependencies
    +    "@react-native-community/masked-view": "0.1.5",
    +    "react-native-safe-area-view": "^1.0.0",
    through trial and error. Should these really be explicit dependencies?

--- App.tsx
+++ App.tsx
@@ -1,13 +1,27 @@
 import React from 'react';
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
+
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-    </View>
-  );
+class HomeScreen extends React.Component {
+  render() {
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>Home Screen</Text>
+      </View>
+    );
+  }
 }
+
+const AppNavigator = createStackNavigator({
+  Home: {
+    screen: HomeScreen,
+  },
+});
+
+
+export default createAppContainer(AppNavigator);
 
 const styles = StyleSheet.create({
   container: {
