a74a40f718135489f2e01a286322faface5cc642
jav <javier@ubillos.org>
2020-01-11 23:05:14 +0100

    Added modal box with date & time -pickers.

--- Screens/DateTimePickerScreen.tsx
+++ Screens/DateTimePickerScreen.tsx
@@ -1,26 +1,37 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { NavigationStackProp } from 'react-navigation-stack';
-import { AppContext, setRealtimeClockMode, updateClock } from '../Context/Context';
+import { AppContext, updateClock } from '../Context/Context';
 
-import { CurrentTime } from '../Components/CurrentTime';
-
-import { Text, View, Button, Platform } from 'react-native';
-import DateTimePicker ,{IOSNativeProps, AndroidNativeProps} from '@react-native-community/datetimepicker';
+import { Text, View, Button } from 'react-native';
+import DateTimePicker from '@react-native-community/datetimepicker';
 
 interface Props {
-  navigation: NavigationStackProp<{}>;
+  navigation: NavigationStackProp<{ mode: string }>;
 }
 
-export const DateTimePickerScreen = (props:Props) => {
+export const DateTimePickerScreen = (props: Props) => {
+
+  const { state, dispatch } = React.useContext(AppContext);
+  const { time } = state;
+
+  const setDate = (e, date) => {
+    dispatch(updateClock(date));
+  }
 
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text style={{ fontSize: 30 }}>This is a modal!</Text>
+      <Text>Set {props.navigation.getParam('mode')}</Text>
+      <View style={{ alignSelf: "stretch" }} >
+        <DateTimePicker value={time}
+          mode={props.navigation.getParam('mode')}
+          is24Hour={true}
+          display="default"
+          onChange={(e, d) => setDate(e, d)} />
+      </View>
       <Button
         onPress={() => props.navigation.goBack()}
         title="Dismiss"
       />
     </View>
   );
-
 }
--- Screens/EditTimeScreen.tsx
+++ Screens/EditTimeScreen.tsx
@@ -1,12 +1,11 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { NavigationStackProp } from 'react-navigation-stack';
 
-import { AppContext, setRealtimeClockMode, updateClock } from '../Context/Context';
+import { AppContext, setRealtimeClockMode } from '../Context/Context';
 
 import { CurrentTime } from '../Components/CurrentTime';
 
-import { Text, View, Switch, Button, Platform } from 'react-native';
-import DateTimePicker, { IOSNativeProps, AndroidNativeProps } from '@react-native-community/datetimepicker';
+import { Text, View, Switch, Button } from 'react-native';
 
 interface Props {
     navigation: NavigationStackProp<{}>;
@@ -14,47 +13,21 @@ interface Props {
 
 export const EditTimeScreen = (props: Props) => {
     const { state, dispatch } = React.useContext(AppContext);
-
     const { time } = state;
-
-    const [mode, setMode] = useState<IOSNativeProps['mode'] | AndroidNativeProps['mode']>('date');
-    const [show, setShow] = useState(false);
-
-    const showPicker = (timeOrDate: IOSNativeProps['mode'] | AndroidNativeProps['mode']) => {
-        setMode(timeOrDate);
-        setShow(true); // possible race condition? I'm betting on that setShow() never executes noticably before setMode()
-    }
-
-    const setDate = (event, date) => {
-        date = date || this.state.date;
-        setShow(Platform.OS === 'ios' ? true : false)
-        dispatch(updateClock(date));
-    }
 
     return (
         <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
             <Text>
                 Edit Time Screen
             </Text>
-            <Switch onValueChange={e => dispatch(setRealtimeClockMode(e))} value={state.realtimeClockMode} />
             <CurrentTime currentTime={time} />
 
-            <View>
-                <View>
-                    <Button onPress={() => props.navigation.navigate('MyModal')} title="Show date picker!" />
-                </View>
-                <View>
-                    <Button onPress={() => props.navigation.navigate('MyModal')} title="Show time picker!" />
-                </View>
-                {show && <DateTimePicker value={time}
-                    mode={mode}
-                    is24Hour={true}
-                    display="default"
-                    onChange={(e, d) => setDate(e, d)} />
-                }
+            <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+                <Text>Realtime mode </Text>
+                <Switch onValueChange={e => dispatch(setRealtimeClockMode(e))} value={state.realtimeClockMode} />
             </View>
-
-
+            <Button onPress={() => props.navigation.navigate('MyModal', { mode: 'date' })} title="Show date picker!" />
+            <Button onPress={() => props.navigation.navigate('MyModal', { mode: 'time' })} title="Show time picker!" />
         </View>
     );
 }
