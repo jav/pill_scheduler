99088fa653aae3cd5a90c76a0806dbbc6782aeea
jav <javier@ubillos.org>
2020-01-28 17:58:26 +0100

    Minor stylistic change in AdministrationList.

--- src/Types/AdministrationList.ts
+++ src/Types/AdministrationList.ts
@@ -1,4 +1,3 @@
-import Pill from './Pill';
 import Administration from './Administration';
 
 class AdministrationList {
@@ -21,16 +20,16 @@ class AdministrationList {
     }
 
     // Gets an interval from time (open) to time (closed)
-    getInterval(from: Date, to: Date) {
-        if (from > to) {
+    getInterval(_from: Date, _to: Date) {
+        if (_from > _to) {
             let tmpDate;
-            tmpDate = from;
-            from = to;
-            to = tmpDate;
+            tmpDate = _from;
+            _from = _to;
+            _to = tmpDate;
         }
         return new AdministrationList(
             this.administrationList.filter(
-                (a) => from < a.time && a.time <= to
+                (a) => _from < a.time && a.time <= _to
             )
         );
     }
