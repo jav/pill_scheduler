eac6aacba64e3ae8127027d0c4cba99772c323f3
jav <javier@ubillos.org>
2020-02-02 23:18:45 +0100

    Added 'PillsDB' class.

--- src/Types/Pill.ts
+++ src/Types/Pill.ts
@@ -22,3 +22,58 @@ export class Pill {
         return this.activeSubstance !== Substance.PARACETAMOL
     }
 }
+
+class PillsDB {
+    pillsDB: Pill[];
+
+    constructor() {
+        this.pillsDB = [
+            new Pill({
+                name: 'Acetylicacid',
+                substance: Substance.ACETYLICACID,
+                doseList: [250, 500, 750, 1000, 1250, 1500, 1750, 2000],
+                defaultDose: 1000
+            }),
+            new Pill({
+                name: 'Diklofenak',
+                substance: Substance.DIKLOFENAK,
+                doseList: [25, 50, 75, 100],
+                defaultDose: 25
+            }),
+            new Pill({
+                name: 'Ibuprofen',
+                substance: Substance.IBUPROFEN,
+                doseList: [100, 200, 300, 400, 500, 600, 700, 800],
+                defaultDose: 400
+            }),
+            new Pill({
+                name: 'Paracetamol',
+                substance: Substance.PARACETAMOL,
+                doseList: [250, 325, 500, 650, 750, 1000],
+                defaultDose: 1000
+            })
+        ]
+    }
+
+    getPill(_name): Pill | null {
+        const filteredList = this.pillsDB.filter((p) => p.name === _name);
+        if (filteredList.length <= 0)
+            return null;
+        return filteredList[0]
+    }
+
+    getAllParacetamol() {
+        return this.pillsDB.filter((p) => p.substance === Substance.PARACETAMOL);
+    }
+
+    getAllNSAID() {
+        console.log("getAllNSAID() will return: ", this.pillsDB.filter(
+            (p) => [Substance.ACETYLICACID, Substance.DIKLOFENAK, Substance.IBUPROFEN].includes(p.activeSubstance))
+        );
+        console.log("having filtered the list:", this.pillsDB);
+        return this.pillsDB.filter(
+            (p) => [Substance.ACETYLICACID, Substance.DIKLOFENAK, Substance.IBUPROFEN].includes(p.activeSubstance));
+    }
+}
+
+export const pillsDB = new PillsDB();
