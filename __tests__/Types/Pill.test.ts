5599b1ebfc7432a84dbdc5e0f0b49076b3e08004
jav <javier@ubillos.org>
2020-02-07 08:44:01 +0100

    Update Pills.test.ts to use pillsDB.

--- __tests__/Types/Pill.test.ts
+++ __tests__/Types/Pill.test.ts
@@ -1,23 +1,23 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
 
-import {Pill, Substance} from '../../src/Types/Pill';
+import {pillsDB, Pill} from '../../src/Types/Pill';
 
 describe('Pill ', () => {
   it('isParacetamol() returns true on PARACETAMOL', () => {
-    const pill = new Pill("name", Substance.PARACETAMOL);
+    const pill = pillsDB.getPill('Paracetamol');
     expect(pill.isParacetamol()).toEqual(true);
   });
   it('isParacetamol() returns false on IBUPROFEN', () => {
-    const pill = new Pill("name", Substance.IBUPROFEN);
+    const pill = pillsDB.getPill('Ibuprofen');
     expect(pill.isParacetamol()).toEqual(false);
   });
   it('isNSAID() returns false on PARACETAMOL', () => {
-    const pill = new Pill("name", Substance.PARACETAMOL);
+    const pill = pillsDB.getPill('Paracetamol');
     expect(pill.isNSAID()).toEqual(false);
   });
   it('isNSAID() returns true on IBUPROFEN', () => {
-    const pill = new Pill("name", Substance.IBUPROFEN);
+    const pill = pillsDB.getPill('Ibuprofen');
     expect(pill.isParacetamol()).toEqual(false);
   });
 });
