76ecbc92510a18d56b686419ea94772ee7c01982
jav <javier@ubillos.org>
2020-02-07 08:44:40 +0100

    Removed unused iports from Administration.test.ts.

--- __tests__/Types/Administration.test.ts
+++ __tests__/Types/Administration.test.ts
@@ -1,9 +1,6 @@
 import React from 'react';
-import renderer from 'react-test-renderer';
 
 import Administration from '../../src/Types/Administration';
-import {Pill, Substance} from '../../src/Types/Pill';
-import { exportAllDeclaration } from '@babel/types';
 
 describe('Administration', () => {
   it('??', () => {
